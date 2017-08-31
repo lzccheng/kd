881ad25056d85f453b70a252f8e2f04f572f5e98
Richard Wang <wangleihd@gmail.com>
2017-08-31 09:35:38 +0800

    update for teach use

--- collections/index.js
+++ collections/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://oc:open@ds151153.mlab.com:51153/oc';
+// const url = 'mongodb://oc:open@ds151153.mlab.com:51153/oc';
+const url = 'mongodb://127.0.0.1:27017/open';
 
 mongoose.connect(url);
 
